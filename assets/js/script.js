9ff69005dc6da7d12159b80f3d919f5258b65659
Alec Hunter <alecryanhunter@gmail.com>
2023-04-02 00:44:40 -0700

    added button and basic timer

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,36 @@
-console.log("Hello!")
+// ACCEPTANCE CRITERIA
+// Begin the game by clicking a start button
+// This starts a timer
+// First question appears immediately
+// When questions are answered, the next shows up
+// If an answer is incorrect, it deducts time
+// Game is finished when all questions are answered or timer reaches 0
+// Can save your initials and score
+    // Do this use local storage
+
+// Store questions in one array, with answers in a second array (of arrays), with indexes matching
+
+var timer = document.querySelector("#timer");
+var timeLeft = 10
+
+function countdown() {
+    timeLeft = 10
+    timer.textContent = timeLeft + " seconds left";
+    var gameTimer = setInterval(function(){
+        timeLeft--
+        timer.textContent = timeLeft + " seconds left";
+        if(timeLeft===0) {
+            clearInterval(gameTimer)
+            timer.textContent = "Time's Up!";
+        }
+    },100)
+}
+
+// Button for starting game
+var startBtn = document.querySelector("#start");
+startBtn.addEventListener("click", function() {
+    // Start timer
+    // Get rid of button and render question and answers
+    console.log("The button works!")
+    countdown();
+})
