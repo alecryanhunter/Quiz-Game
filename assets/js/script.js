fe378b2553197f543b39e1b09640c0bc9aa0a00f
Alec Hunter <alecryanhunter@gmail.com>
2023-04-02 01:39:53 -0700

    added answer selection and question iteration prototype

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,23 +10,49 @@
 
 
 // Store questions in one array, with answers in a second array (of arrays), with indexes matching
+// Separate array to denote which option is the correct one? Or always have index 0 be the answer and randomize them?
 var questions = ["Question 1","Question 2","Question 3"];
-var options = [["1A","1B","1C","1D"],["2A","2B","2C","2D"],["3A","3B","3C","3D"]]
+var options = [["1A","1B","1C","1D"],["2A","2B","2C","2D"],["3A","3B","3C","3D"]];
+var correct = [2,0,3];
+
+var completed = ""
+// This function creates an array of x's the same length as the questions, which I can then use to check if that specific question index has been done before, if I implement randomization
+function completeCreate() {
+    for (i=0;i<questions.length;i++) {
+        completed = completed+"x"
+        console.log(completed)
+    }
+    completed = completed.split(``);
+}
+completeCreate()
+
+// This function is for debugging only, making sure that my questions and options are always the same length
+function lengthCheck() {
+    if (questions.length === options.length) {
+    } else {
+        console.log("QUESTION/OPTIONS ARRAY LENGTH DISCREPANCY")
+    }
+}
+lengthCheck();
 
 var startBtn = document.querySelector("#start");
 var timer = document.querySelector("#timer");
 var questionField = document.querySelector("#question")
 var optionsField = document.querySelector("#options")
 
-var timeLeft = 10
+var timeLeft
+var next = 0
 
-// Function for retrieving and printing the questions
+// Function for retrieving and printing the questions. Assigns them an index number
 function renderQs() {
+    // TODO: If statement saying if next exceeds the question length, it goes to game over
+    optionsField.innerHTML=""
     startBtn.setAttribute("style","display:none");
-    questionField.textContent = questions[1]
-    for (i=0;i<options[1].length;i++) {
+    questionField.textContent = questions[next]
+    for (i=0;i<options[next].length;i++) {
         var li = document.createElement("li")
-        li.textContent=options[1][i];
+        li.textContent=options[next][i];
+        li.dataset.index=i;
         optionsField.appendChild(li)
     }
 }
@@ -34,13 +60,14 @@ function renderQs() {
 optionsField.addEventListener("click",function(event){
     target = event.target
     if (target.matches("li")) {
-        
+        next++
+        renderQs();
     }
 })
 
 // Countdown timer function
 function countdown() {
-    timeLeft = 10
+    timeLeft = 100
     timer.textContent = timeLeft + " seconds left";
     var gameTimer = setInterval(function(){
         timeLeft--
@@ -49,7 +76,7 @@ function countdown() {
             clearInterval(gameTimer)
             timer.textContent = "Time's Up!";
         }
-    },1000000)
+    },1000)
 }
 
 // Button for starting game
